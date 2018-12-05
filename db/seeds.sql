fd69516332fdc2900da92f985fe3a144ecbe1972
Chad Davis <ccdavis2k@gmail.com>
2018-12-04 20:05:47 -0500

    Main functionality is working except for Put Method

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Bypass Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger with Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Black Bean Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES (' Mushroom Swiss Burger', true);
